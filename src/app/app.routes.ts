dbec5a837ab29d24b2b455a249bed115558f7588
Jyothi-61 <adireddy.jyothi@cognizant.com>
2025-09-29 16:05:21 +0530

    updated front-end

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login';
-import { Welcome } from './welcome/welcome';
 import { Adminpro } from './adminpro/adminpro';
 import { Pending } from './pending/pending';
 
 export const routes: Routes = [{
         path:'login',component:LoginComponent
     },{
-        path:'',component:Welcome
+        path:'',component:LoginComponent
     },
     {path:'adminpro',component:Adminpro},
-    {path:'pending',component:Pending}
+    {path:'pending',component:Pending},
 ];
